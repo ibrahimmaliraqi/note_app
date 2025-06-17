7fc7940a94a78731cee17880ebee6cbd12acb5e4
ibrahim mohammed <ibrahom0780@gmail.com>
2025-06-17 15:03:58 +0300

    add not hive to cubit

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,9 +1,14 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:note_app/constants.dart';
 import 'package:note_app/cubits/add_note_cubit/add_note_satate.dart';
 import 'package:note_app/mdels/note_model.dart';
 
 class AddNoteCubit extends Cubit<AddNoteSatate> {
   AddNoteCubit() : super(AddNoteInstite());
 
-  addNote(NoteModel note) {}
+  addNote(NoteModel note) async {
+    var addNote = Hive.box<NoteModel>(kHiveBox);
+    await addNote.add(note);
+  }
 }
